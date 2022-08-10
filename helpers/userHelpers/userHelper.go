a9c029d9bdca8812b77592c566d93d9e20b7d40f
Vaishakh GK <vaishakhgk2006@gmail.com>
2022-08-10 22:00:34 +0530

    signup issue fix

--- helpers/userHelpers/userHelper.go
+++ helpers/userHelpers/userHelper.go
@@ -12,21 +12,21 @@ var ctx = databaseConnection.Ctx
 
 // user singup
 func DoUserSignUp(body []byte) []byte {
-	var user models.User
+	var user map[string]interface{}
 	// decoding request body
 	helpers.DecodeJson(body, &user)
 	// check if email is availble
-	var emailAvailble = IsEmailAvailble(user.Email)
+	var emailAvailble = IsEmailAvailble(user["email"].(string))
 	if !emailAvailble {
 		var res = helpers.EncodeJson(map[string]interface{}{"status": false, "reason": "Email in use"})
 		return res
 	}
 	// hashing password
-	user.Password = hashPassword(user.Password)
+	user["password"] = hashPassword(user["password"].(string))
 	// inserting user into database
 	var insertedID = insertUser(user)
 	// response sending to user with user details
-	user.Id = insertedID.(primitive.ObjectID)
+	user["id"] = insertedID.(primitive.ObjectID)
 	var encodedUser = helpers.EncodeJson(map[string]interface{}{"status": true})
 	return encodedUser
 }
