89561a366d6dd3e2c47debf113415f9f7ab55192
Vaishakh GK <vaishakhgk2006@gmail.com>
2022-09-25 21:06:57 +0530

    change in product model, in json format id changed to _id

--- models/productModel.go
+++ models/productModel.go
@@ -3,7 +3,7 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Product struct {
-	Id          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Id          primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
 	Name        string             `json:"name" bson:"name"`
 	Type        string             `json:"type" bson:"type"`
 	Price       int                `json:"price" bson:"price"`
